536a312a57045a2d345f419f7deb6e0cef29dfa7
CameronDunning <dunningcameron@gmail.com>
2019-10-02 22:43:02 -0400

    Separate managing state functions

--- src/components/Application.js
+++ src/components/Application.js
@@ -10,63 +10,18 @@ import {
   getInterview
 } from "helpers/selectors";
 
-const Application = props => {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {}
-  });
+import { useApplicationData } from "hooks/useApplicationData";
 
-  const setDay = day => setState({ ...state, day });
-  // const setDays = days => setState(prev => ({ ...prev, days }));
-
-  useEffect(() => {
-    Promise.all([
-      axios.get(`/api/days`),
-      axios.get(`/api/appointments`),
-      axios.get(`/api/interviewers`)
-    ]).then(all => {
-      setState(prev => ({
-        ...state,
-        days: all[0].data,
-        appointments: all[1].data,
-        interviewers: all[2].data
-      }));
-    });
-  }, []);
+const Application = () => {
+  const {
+    state,
+    setDay,
+    bookInterview,
+    deleteInterview
+  } = useApplicationData();
 
   const appointments = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state, state.day);
-
-  const bookInterview = (id, interview) => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    console.log(appointment);
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    console.log(appointments);
-    return axios
-      .put(`/api/appointments/${id}`, appointment)
-      .then(() => setState({ ...state, appointments }));
-  };
-
-  const deleteInterview = id => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios.delete(`/api/appointments/${id}`).then(() => {
-      setState({ ...state, appointments });
-    });
-  };
 
   const schedule = appointments.map(appointment => {
     const interview = getInterview(state, appointment.interview);
