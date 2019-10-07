a5c6b0ab482848c62bfcf0c1eca70f9e05a850d9
CameronDunning <dunningcameron@gmail.com>
2019-10-07 19:46:52 -0400

    Add reducer test

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -4,32 +4,12 @@ import {
   updateSpotsRemaining,
   createNewAppointments
 } from "helpers/dataUpdaters";
-
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case SET_DAY:
-      return { ...state, day: action.day };
-    case SET_APPLICATION_DATA:
-      return {
-        ...state,
-        ...action.data
-      };
-    case SET_INTERVIEW:
-      return {
-        ...state,
-        appointments: action.appointments,
-        days: action.days
-      };
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
-  }
-};
+import {
+  reducer,
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW
+} from "reducers/application";
 
 const useApplicationData = () => {
   const [state, dispatch] = useReducer(reducer, {
